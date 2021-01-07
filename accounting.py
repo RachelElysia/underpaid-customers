cc64aaad419bec4ec8c0579865a7adec4c7ef0aa
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-01-07 01:12:25 +0000

    Correct Print Out

--- accounting.py
+++ accounting.py
@@ -10,11 +10,18 @@ for line in the_file:
     customer_name = customer_info[1]
     customer_expected = float(customer_info[2]) * melon_cost
     customer_paid = float(customer_info[3].rstrip())
+    customer_first_name = customer_name.split(" ")[0]
+    difference = customer_expected - customer_paid
 
+    #catch if customer did not pay correctly and print statement
     if customer_expected != customer_paid:
         print(f"{customer_name} paid ${customer_paid},",
             f"expected ${customer_expected:.2f}"
         )
+    
+    #underpaid additional statement
+    if customer_paid < customer_expected:
+        print(f"{customer_first_name} has underpaid for their melons by {difference}.")
     
 
 # customer1_expected = customer1_melons * melon_cost
